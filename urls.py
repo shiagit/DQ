2d0c8879a508c79dbbf5cb9d12f4c868f28b56c9
shia <30616724+shiagit@users.noreply.github.com>
2017-08-03 17:10:35 +0800

    django

---
+++ urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+
+urlpatterns = [
+    url(r'^like/$', views.image_like, name='like'),
+    url(r'^create/$', views.image_create, name='create'),
+    url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', views.image_detail, name='detail'),
+    
+    url(r'^$', views.image_list, name='list'),
+]
